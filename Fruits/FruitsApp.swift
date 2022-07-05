e5db90ecff7ea94d3c07c8f00f6eb091d46ccab9
Emmanuel Onuoha <emanonuoha@gmail.com>
2022-07-05 09:51:28 -0500

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Emmanuel Onouha on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
